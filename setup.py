52fa087ec3d82b4d8223552ffbc3977b44cc3506
Nicolas Bigot <nicbigot@gmail.com>
2017-07-16 00:22:10 +0200

    setup for pypi

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name='python-undoable-transaction',
+    version='0.1.0',
+    packages=['python-undoable-transaction'],
+    zip_safe=True,
+    author='Nicolas Bigot',
+    author_email='nicbigot@gmail.com',
+    description='Undoable transaction',
+    long_description=long_description='Undoable transaction',
+    license='MIT',
+    keywords=['undo', 'undoable', 'transaction'],
+    url='https://github.com/nbigot/python-undoable-transaction',
+    platforms='any',
+    tests_require=[
+        'pytest>=2.5.0'
+    ],
+    classifiers="""\
+Development Status :: 4 - Beta
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.0
+Programming Language :: Python :: 3.1
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Database
+Topic :: Software Development :: Libraries :: Application Frameworks
+Topic :: Software Development :: Libraries :: Python Modules""".splitlines()
+)
